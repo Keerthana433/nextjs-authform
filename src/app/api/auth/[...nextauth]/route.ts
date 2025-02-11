2d4de70569b1bbded5c1bd547cc148e45e17acf2
Keerthana433 <keerthana.433.arun@gmail.com>
2025-02-11 12:38:44 +0530

    created simple signin and signup using shadcn/ui and tailwind

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,68 @@
+import NextAuth, { Awaitable, RequestInternal } from "next-auth";
+import User from "@/models/user";
+import connectToDatabase from "@/lib/mongodb";
+import bcrypt from 'bcryptjs';
+import CredentialsProvider from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github"
+
+const handler = NextAuth({
+    session: {
+        strategy: 'jwt',
+    },
+    providers: [
+        Github({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {},
+                password: {}
+            },
+            async authorize(credentials) {
+               try{
+                await connectToDatabase();
+                const user = await User.findOne({ email: credentials?.email })
+                if(!user){
+                    throw new Error("")
+                }
+                const isValidPassword = await bcrypt.compare(
+                    credentials?.password ?? "", user.password as string
+                )
+                if(!isValidPassword) {
+                    throw new Error("")
+                }
+                return user;
+               } catch {
+                return null
+               } 
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({token, user}) {
+            if(user){
+                token.id = user.id;
+                token.email = user.email;
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if(token){
+                session.user = {
+                    email: token.email,
+                    name: token.name,
+                    image: token.picture
+                }
+            }
+            return session
+        }
+    },
+    pages: {
+        signIn: "/sign-in"
+    },
+    secret: process.env.NEXTAUTH_SECRET
+    })
+
+    export { handler as GET, handler as POST };
